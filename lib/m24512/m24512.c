95b0223f4cf78c397a25d2b5e46555b148032532
M1cha <sigmaepsilon92@gmail.com>
2019-03-24 00:57:00 +0100

    lib/m24512: add support for reading from any offset with any size

--- lib/m24512/m24512.c
+++ lib/m24512/m24512.c
@@ -6,6 +6,13 @@
 
 #define M24512_ADDRESS 0x50
 
+// while technically we could read up to 128 bytes,
+// some i2c adapters don't support such big transactions
+#define MAX_READ_SIZE 64
+
+#define MIN(a,b) ((a)<(b) ? (a) : (b))
+#define ROUNDDOWN(a, b) ((a) & ~((b)-1))
+
 int m24512_create(struct m24512 *dev, struct crossi2c_bus *i2cbus) {
     memset(dev, 0, sizeof(*dev));
 
@@ -19,12 +26,29 @@ int m24512_destroy(struct m24512 *dev) {
 }
 
 int m24512_read(struct m24512 *dev, uint16_t addr, void *data, size_t len) {
+    int rc;
     uint8_t addrbuf[2];
 
-    addrbuf[0] = (addr >> 8) & 0xff;
-    addrbuf[1] = (addr) & 0xff;
+    while (len) {
+        uint8_t msb = (addr >> 8) & 0xff;
+        uint8_t lsb = addr & 0xff;
+        // never read beyond the chunk boundary
+        size_t toread = MIN(len, (size_t)MAX_READ_SIZE - (lsb - ROUNDDOWN(lsb, MAX_READ_SIZE)));
 
-    return crossi2c_write_read(dev->i2cbus, M24512_ADDRESS,
-        addrbuf, 2,
-        data, len);
+        addrbuf[0] = msb;
+        addrbuf[1] = lsb;
+
+        rc = crossi2c_write_read(dev->i2cbus, M24512_ADDRESS,
+            addrbuf, 2,
+            data, toread);
+        if (rc) {
+            return -1;
+        }
+
+        data += toread;
+        addr += toread;
+        len -= toread;
+    }
+
+    return 0;
 }
