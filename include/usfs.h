46857156af1d02a7946ea6b7af8fe067bfad4cee
M1cha <sigmaepsilon92@gmail.com>
2019-06-16 16:54:20 +0200

    usfs: add usfs_get_us

--- include/usfs.h
+++ include/usfs.h
@@ -6,7 +6,21 @@
     #define usfs_usleep(x) k_sleep((x))
 #elif defined(__unix__)
     #include <unistd.h>
+    #include <time.h>
+
     #define usfs_usleep(x) usleep((x))
+
+    static inline uint64_t usfs_get_us(void) {
+        int rc;
+        struct timespec ts;
+
+        rc = clock_gettime(CLOCK_MONOTONIC, &ts);
+        if (rc) {
+            return 0;
+        }
+
+        return ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
+    }
 #elif defined(ESP_PLATFORM)
     #include <esp32/rom/ets_sys.h>
     #define usfs_usleep(x) ets_delay_us((x))
